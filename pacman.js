efb434774ad17aec6f5b324cb88f7aa6fcb325ea
Roopesh <rrattan.89@gmail.com>
2017-07-10 21:04:19 -0400

    step 5a: power-pellets

--- pacman.js
+++ pacman.js
@@ -1,6 +1,7 @@
 // Setup initial game stats
 var score = 0;
 var lives = 2;
+var powerPellets = 4;
 
 
 // Define your ghosts here
@@ -55,7 +56,7 @@ function clearScreen() {
 }
 
 function displayStats() {
-  console.log('Score: ' + score + '     Lives: ' + lives);
+  console.log('Score: ' + score + '     Lives: ' + lives + ' Power-Pellets:' + powerPellets);
 }
 
 function displayMenu() {
